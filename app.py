720013e5700291bfb02749e7734d776cb1403802
rudolphleon <rudolphleon@gmail.com>
2024-04-17 21:15:06 -0700

    Semi-Working Tracker Incrementing incrrectly

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+import sqlite3
 import time
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///timetracker.db'
-db = SQLAlchemy(app)
-
-class Project(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True, nullable=False)
-    total_time = db.Column(db.Integer, default=0)
+db_path = '/home/leon/workspace/github.com/rudolphleon/time_tracker/timetracker.db'
 
 @app.route('/')
 def index():
@@ -17,35 +11,38 @@ def index():
 
 @app.route('/start_timer', methods=['POST'])
 def start_timer():
-    project_name = request.form['project_name']
+    project_name = request.form['project_name'].strip()
     topic_name = request.form['topic_name']
-    
-    # Update total time for the project in the database
-    project = Project.query.filter_by(name=project_name).first()
-    if project:
-        project.total_time += 1
-    else:
-        new_project = Project(name=project_name, total_time=1)
-        db.session.add(new_project)
-    db.session.commit()
-    
-    # Redirect to the timer display route
-    return redirect(url_for('timer_display', project=project_name, topic=topic_name))
 
+    with sqlite3.connect(db_path) as conn:
+        cursor = conn.cursor()
+        
+        # Check if project already exists in the database
+        cursor.execute("SELECT * FROM project WHERE name=?", (project_name,))
+        project = cursor.fetchone()
+
+        if project:
+            # If project exists, update the total time
+            cursor.execute("UPDATE project SET total_time = total_time + ? WHERE name=?", (1, project_name))
+        else:
+            # If project does not exist, insert a new row
+            cursor.execute("INSERT INTO project (name, total_time) VALUES (?, ?)", (project_name, 1))
+
+        conn.commit()
+
+    return redirect(url_for('timer_display', project=project_name, topic=topic_name))
 
 @app.route('/timer_display')
 def timer_display():
     project_name = request.args.get('project')
     topic_name = request.args.get('topic')
-    
-    # Retrieve total time for the project from the database
-    project = Project.query.filter_by(name=project_name).first()
-    total_time = project.total_time if project else 0
-    
-    # Render the timer display template with the project, topic, and total time
+
+    with sqlite3.connect(db_path) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT total_time FROM project WHERE name=?", (project_name,))
+        total_time = cursor.fetchone()[0]
+
     return render_template('timer_display.html', project=project_name, topic=topic_name, total_time=total_time)
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
